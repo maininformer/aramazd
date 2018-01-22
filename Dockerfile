865e306995e94268a4f4405580fa937e29c7d005
plumSemPy <emin@berkeley.edu>
2018-01-21 22:38:01 -0800

    update docker files to make neo4j work

--- Dockerfile
+++ Dockerfile
@@ -1,1 +1,5 @@
 FROM python:3
+
+WORKDIR /aramazd/
+COPY . .
+RUN pip install -r requirements.txt
--- src/eye.py
+++ src/eye.py
@@ -2,4 +2,7 @@ from PIL import Image
 import imagehash
 hash = imagehash.whash(Image.open('./images/purple.jpg'))
 other_hash = imagehash.whash(Image.open('./images/yellow.jpg'))
-print(hash-other_hash)
+
+from py2neo import Graph, authenticate
+
+graph = Graph("http://neo4j:pass@neo4j:7474/db/data/")
